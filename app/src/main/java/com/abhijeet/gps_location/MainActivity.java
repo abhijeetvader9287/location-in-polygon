0a9aece19f7602cc61580606929e23f883b2decf
Abhijeet <abhijeet@twigsoftwares.com>
2017-01-11 14:50:00 +0530

    code cleanup

--- app/src/main/java/com/abhijeet/gps_location/MainActivity.java
+++ app/src/main/java/com/abhijeet/gps_location/MainActivity.java
@@ -1,30 +1,12 @@
 package com.abhijeet.gps_location;
-import android.Manifest;
-import android.app.Notification;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Toast;
 
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import cn.pedant.SweetAlert.SweetAlertDialog;
 public class MainActivity extends AppCompatActivity {
-    GPSTracker gps;
+    private GPSTracker gps;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
